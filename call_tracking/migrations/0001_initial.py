99f70e8090859248878117be66cf8a469d5594d8
Andrew T. Baker <abaker@twilio.com>
2015-08-12 19:54:11 -0400

    Flattening migrations

--- call_tracking/migrations/0001_initial.py
+++ call_tracking/migrations/0001_initial.py
@@ -14,9 +14,21 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='LeadSource',
             fields=[
-                ('id', models.AutoField(verbose_name='ID', primary_key=True, serialize=False, auto_created=True)),
-                ('name', models.CharField(max_length=100)),
+                ('id', models.AutoField(verbose_name='ID', primary_key=True, auto_created=True, serialize=False)),
+                ('name', models.CharField(max_length=100, blank=True)),
+                ('incoming_number', phonenumber_field.modelfields.PhoneNumberField(max_length=128, unique=True)),
+                ('forwarding_number', phonenumber_field.modelfields.PhoneNumberField(max_length=128, blank=True)),
+            ],
+        ),
+        migrations.CreateModel(
+            name='Lead',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', primary_key=True, auto_created=True, serialize=False)),
                 ('phone_number', phonenumber_field.modelfields.PhoneNumberField(max_length=128)),
+                ('timestamp', models.DateTimeField(auto_now_add=True)),
+                ('city', models.CharField(max_length=100)),
+                ('state', models.CharField(max_length=2)),
+                ('source', models.ForeignKey(to='call_tracking.LeadSource')),
             ],
         ),
     ]
